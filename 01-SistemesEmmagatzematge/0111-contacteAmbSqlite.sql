9cebe94326afc71bfe552052d437178cd6b43804
Nacho Cabanes <ncabanes@users.noreply.github.com>
2022-09-21 09:57:34 +0200

    Exercici 0111 - Contacte amb SQLite

---
+++ 01-SistemesEmmagatzematge/0111-contacteAmbSqlite.sql
@@ -0,0 +1,6 @@
+.open amistades
+CREATE TABLE amigos (nombre VARCHAR(50), email VARCHAR(100));
+INSERT INTO amigos VALUES ("Jose", "josito@gmail.com");
+INSERT INTO amigos VALUES ("Marta", "marta@gmail.com");
+INSERT INTO amigos VALUES ("Clara", "Clara@gmail.com");
+SELECT * FROM amigos;
