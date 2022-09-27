9e6fa1459086e23f8636d77a1926d74d30958dc5
Nacho Cabanes <ncabanes@users.noreply.github.com>
2022-09-27 18:13:58 +0200

    Exercici 0204 - Mascotes + NULL

---
+++ 0203-ModelEntitatRelacioYModelRelacional/0204-null.sql
@@ -0,0 +1,5 @@
+INSERT INTO mascotas3 VALUES (3, "Daisy", "");  -- Poco razonable
+
+INSERT INTO mascotas3 VALUES (4, "Thor", NULL); -- NULL en vez de un valor
+INSERT INTO mascotas3 (codigo, nombre) VALUES (5, "Nieve"); -- Sólo algunos campos
+INSERT INTO mascotas3 (nombre, codigo) VALUES ("Golfo", 6); -- Orden cambiado
