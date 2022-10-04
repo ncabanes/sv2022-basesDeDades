6734ef8b9f1f0a0e1583362181888d053d0571a3
Nacho Cabanes <ncabanes@users.noreply.github.com>
2022-10-04 14:17:51 +0200

    Exercici 0217 - Equips + clau aliena

---
+++ 0203-ModelEntitatRelacioYModelRelacional/0217-equipsForana.sql
@@ -0,0 +1,42 @@
+.open futbol2;
+
+create table equipo(
+    codigo numeric(2) primary key,
+    nombre varchar(30)
+);
+
+create table jugador(
+    codigo varchar(10) primary key,
+    nombre varchar(30),
+    codigo_equipo numeric(2),
+    foreign key(codigo_equipo) references equipo(codigo)
+);
+
+insert into equipo values(
+    1, "Real Madrid"
+);
+insert into equipo values(
+    2, "Atletico Madrid"
+);
+   
+insert into jugador values(
+    "ben", "Benzema", 1
+);
+insert into jugador values(
+    "car", "Carrasco", 2
+);
+insert into jugador values(
+    "vin", "Vinicius", 1
+);
+insert into jugador values(
+    "obl", "Oblak", 2
+);
+
+select jugador.nombre, equipo.nombre 
+from equipo, jugador 
+where codigo_equipo = equipo.codigo;
+
+-- Benzema|Real Madrid
+-- Carrasco|Atletico Madrid
+-- Vinicius|Real Madrid
+-- Oblak|Atletico Madrid
