983456f00b98ee1444c2cf55455c813394d9d6da
Nacho Cabanes <ncabanes@users.noreply.github.com>
2022-10-21 13:48:44 +0200

    Exercici 0238 - Matrimoni 1:1 Reflexiva - SQL corregit

--- 0203-ModelEntitatRelacioYModelRelacional/0238b-matrimoniReflexiva11.sql
+++ 0203-ModelEntitatRelacioYModelRelacional/0238b-matrimoniReflexiva11.sql
@@ -2,28 +2,38 @@
 -- CP: DNI
 -- CAj: DNIconyuge --> Persona
 
-.open matrimonio11;
+--Casar(Dni1, Dni2)
+-- CP: Dni1
+-- CP: Dni2
+-- CAj: Dni1 --> Persona
+-- CAj: Dni2 --> Persona
+
+
+.open matrimonio11ne;
 
 create table personas(
 	dni varchar(9) primary key,
-	nombre varchar(50), 
-	dniconyuge varchar(9) unique,
-	foreign key (dniconyuge) references personas (dni)
+	nombre varchar(50)
 );
 
-insert into personas values("1", "Alvaro", NULL);
-insert into personas values("2", "Irene", "3");
-insert into personas values("3", "Ramiro", "2");
+create table casar(
+	dni1 varchar(9) primary key,
+	dni2 varchar(9) unique not null,
+	foreign key (dni1) references personas (dni)
+	foreign key (dni2) references personas (dni)
+);
 
-insert into personas values("2", "Irene", "4"); -- Código duplicado
--- Runtime error: UNIQUE constraint failed: personas.dni (19)
+insert into personas values("1", "Alvaro");
+insert into personas values("2", "Irene");
+insert into personas values("3", "Ramiro");
+insert into casar values("2", "3");
 
-insert into personas values("2", "Irene", "2"); -- Cónyuge duplicado
--- Runtime error: UNIQUE constraint failed: personas.dniconyuge (19)
+insert into personas values("2", "1"); -- Código duplicado
+insert into personas values("1", "3"); -- Cónyuge duplicado
+insert into personas values("1", NULL); -- No debe ser NULL
 
 select p1.nombre, p2.nombre
-from personas p1, personas p2
-where p1.dniconyuge = p2.dni;
+from personas p1, personas p2, casar
+where casar.dni1 = p1.dni and casar.dni2 = p2.dni;
 
---Irene|Ramiro
---Ramiro|Irene
+-- Irene|Ramiro
